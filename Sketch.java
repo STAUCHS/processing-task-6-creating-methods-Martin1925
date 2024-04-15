b3a55f5fa83bec82ffe29700bffee0e4606dd750
Martin1925 <Martin.Nguyen25@ycdsbk12.ca>
2024-04-15 13:16:04 -0400

    Code

--- Sketch.java
+++ Sketch.java
@@ -15,14 +15,26 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    
-  }
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 0; intColumn < 30; intColumn++) {
+          Square(intRow, intColumn);
+          
+      }
+    }
+
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 0; intColumn < 30; intColumn++) {
+          Circle(intRow, intColumn);
+      }
+    }
+
 
   /**
    * Description
@@ -33,6 +45,45 @@ public class Sketch extends PApplet {
    * @author 
    */
 
-  
-  
+    }
+
+  private void Square(int intRow, int intColumn) {
+    int intX = 0;
+    int intY = 0;
+    intX = 3 + intRow*30;  // Instead of zero, calculate the proper intX location using 'intRow'
+    intY = 3 + intColumn*30; // Instead of zero, calculate the proper intY location using 'intColumn'
+    fill(125);
+    noStroke();
+    rect(intX, intY, 20, 20);
+
+    int intX2 = 0;
+    int intY2 = 0;
+    intX2 = 3 + intRow*30;  // Instead of zero, calculate the proper intX location using 'intRow'
+    intY2 = 3 + intColumn*30; // Instead of zero, calculate the proper intY location using 'intColumn'
+    fill(175);
+    noStroke();
+    rect(intX2, intY2, 10, 10);
+
+
+  }
+  private void Circle(int intRow, int intColumn) {
+    int intX = 0;
+    int intY = 0;
+    intX = 3 + intRow*25;  // Instead of zero, calculate the proper intX location using 'intRow'
+    intY = 3 + intColumn*45; // Instead of zero, calculate the proper intY location using 'intColumn'
+    fill(255);
+    noStroke();
+    ellipse(intX, intY, 15, 15);
+
+    int intX2 = 0;
+    int intY2 = 0;
+    intX2 = 3 + intRow*25;  // Instead of zero, calculate the proper intX location using 'intRow'
+    intY2 = 3 + intColumn*45; // Instead of zero, calculate the proper intY location using 'intColumn'
+    fill(0);
+    noStroke();
+    ellipse(intX2, intY2, 5, 5);
+
+
+  }
 }
+
